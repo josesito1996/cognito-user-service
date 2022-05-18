19007fbbe7ac1de32ff40c6b9e1c34aa969a4375
josesito1996 <85954217+josesito1996@users.noreply.github.com>
2022-05-18 12:26:58 -0500

    Revert "Develop"

--- src/main/java/com/samy/service/cognitoapp/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.samy.service.cognitoapp.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    private static final long serialVersionUID = 1862618285700942977L;
-    public BadRequestException(String msg) {
-        super(msg);
-    }
-}
